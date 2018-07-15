8cb04087253d7eabc10b503f3bc97df15d13caae
cbdyzj <cbdyzj@jianzhao.org>
2018-07-15 20:59:14 +0800

    Add markdown api

--- router.js
+++ router.js
@@ -1,13 +1,24 @@
 const Router = require('koa-router')
 
+const util = require('util')
+
+const marked = require('marked')
+
 const router = new Router
 
-router.get('/', ctx => {
+router.get('/17', ctx => {
     ctx.body = { hello: 0b00010001 }
 })
 
-router.get('/17', ctx => {
-    ctx.body = '00010001'
+const markdown = util.promisify(marked)
+
+router.post('/markdown', async ctx => {
+    const { body } = ctx.request
+    try {
+        ctx.body = await markdown(body)
+    } catch (error) {
+        ctx.body = error
+    }
 })
 
 router.all('/notify', ctx => {
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+const path = require('path')
+
 const Koa = require('koa')
+const serve = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const logger = require('koa-logger')
-
 const router = require('./router')
 
 const app = new Koa
 
+app.use(serve(path.join(__dirname, 'static')));
+
 app.use(logger())
-app.use(bodyParser())
+
+app.use(bodyParser({ enableTypes: ['json', 'form', 'text'] }))
+
 app.use(router.routes())
 
 app.listen(3000, '127.0.0.1')
