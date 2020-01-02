a96f3ea5063a786228c67e2396c4053418f85317
cbdyzj <cbdyzj@jianzhao.org>
2020-01-02 17:12:22 +0800

    Amend

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
+const { join } = require('path')
 const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.use(express.static(join(__dirname, '..', 'static')))
+
+app.get('/api/hello', (req, res) => {
+    res.end('Hello World!')
 })
 
 app.listen(3000, () => {
