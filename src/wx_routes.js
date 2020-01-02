315992277da5daa278075ee1f97355b40fdd6cfc
cbdyzj <cbdyzj@jianzhao.org>
2020-01-02 20:29:46 +0800

    Amend

--- src/wx_routes.js
+++ src/wx_routes.js
@@ -1,10 +1,23 @@
+const crypto = require('crypto')
 const { Router } = require('express')
+
+const { token } = require('./config')
 
 const wxRouter = Router()
 
-wxRouter.get("/wx",(req,res)=>{
-    console.log(req.query)
-    res.end('wx')
+const sha1Hash = crypto.createHash('sha1')
+
+wxRouter.get("/wx", (req, res) => {
+    const { signature, echostr, timestamp, nonce } = req.query
+
+    const joined = [token, timestamp, nonce].sort().join('')
+    const digest = sha1Hash.update(joined).digest('hex')
+
+    if(digest === signature){
+        res.end(echostr)
+        return
+    }
+    res.end()
 })
 
 module.exports = wxRouter
