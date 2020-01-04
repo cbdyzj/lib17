05e2b7ccb9f8f81f76dbe2fdf48edfd447eea7e4
cbdyzj <cbdyzj@jianzhao.org>
2020-01-04 23:52:09 +0800

    Amend

--- src/wx/handler/text_handler/rules.js
+++ src/wx/handler/text_handler/rules.js
@@ -7,29 +7,25 @@ const {
 
 const rules = [{
     pattern: /^hi$/i,
-    reply() {
-        return 'hi，你好哇！'
-    },
+    reply: 'hi，你好哇！'
 }, {
     pattern: /^在吗？?$/,
-    reply() {
-        return 'nano在的'
-    },
+    reply: 'nano在的',
+}, {
+    pattern: /^你是谁？?$/,
+    reply: '我是nano呀',
 }, {
     pattern: /^nano?$/i,
-    reply() {
-        return '试试输入下列关键词让nano帮你吧：'
-            + '\n' + channels.map(c => c.name).join('\n')
-    }
+    reply: '试试输入下列关键词让nano帮助你吧：' + '\n' + channels.map(c => c.name).join('\n')
 }, {
-    pattern: /^重置频道$/i,
-    reply() {
+    pattern: /^重置频道$/,
+    reply(ctx) {
         ctx.channel.delete(ctx.payload.fromUserName)
         return '已重置'
     }
 }, {
     pattern: /^nano状态$/i,
-    reply() {
+    reply(ctx) {
         return getStatusText()
     }
 }]
@@ -39,7 +35,11 @@ const handlers = []
 handlers.push((ctx, next) => {
     for (const rule of rules) {
         if (rule.pattern.test(ctx.payload.content)) {
-            ctx.text(rule.reply())
+            if (typeof rule.reply === 'string') {
+                ctx.text(rule.reply)
+            } else if (typeof rule.reply === 'function') {
+                ctx.text(rule.reply(ctx))
+            }
             return
         }
     }
