e18905e5db0250b905304ff031efda4ff16f50a5
cbdyzj <cbdyzj@jianzhao.org>
2020-01-04 23:31:33 +0800

    Amend

--- src/util/app_status.js
+++ src/util/app_status.js
@@ -6,17 +6,20 @@ function formatMB(bytes) {
 
 function getAppStatusText() {
     const result = []
-    result.push('version: : ' + process.version)
-    result.push('process uptime: : ' + Math.trunc(process.uptime()) + ' seconds')
+    result.push('version: ' + process.version)
+    result.push('process uptime: ' + Math.trunc(process.uptime()) + ' seconds')
     const memoryUsage = process.memoryUsage()
     result.push('rss: ' + formatMB(memoryUsage.rss))
     result.push('heapTotal: ' + formatMB(memoryUsage.heapTotal))
     result.push('heapUsed: ' + formatMB(memoryUsage.heapUsed))
     result.push('external: ' + formatMB(memoryUsage.external))
-    result.push('os: ' + os.type() + ' ' + os.release())
+    result.push('type: ' + os.type())
+    result.push('release: ' + os.release())
     result.push('platform: ' + os.platform())
     result.push('arch: ' + os.arch())
     result.push('os uptime: ' + os.uptime() + ' seconds')
+    const cpus = os.cpus()
+    result.push('cpus: ' + cpus.length + ' * ' + cpus[0].model)
     result.push('totalmem: ' + formatMB(os.totalmem()))
     result.push('freemem: ' + formatMB(os.freemem()))
 
