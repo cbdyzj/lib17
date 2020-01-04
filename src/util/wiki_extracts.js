205111f0f90834f326eb6a3ad8189d16e8cf4e61
cbdyzj <cbdyzj@jianzhao.org>
2020-01-04 15:17:51 +0800

    Amend

--- src/util/wiki_extracts.js
+++ src/util/wiki_extracts.js
@@ -3,12 +3,25 @@ const axios = require('axios')
 const QUERY_API = 'https://org-jianzhao-payroll.herokuapp.com/wiki/extracts?titles='
 const URL_PREFIX = 'https://zh.m.wikipedia.org/wiki/'
 
-const MAX_LENGTH = 600
+const MAX_LENGTH = 320
 
 function ellipsis(longString = '') {
     if (longString.length <= MAX_LENGTH) {
         return longString
     }
+    const stringItems = longString.split('\n')
+    let result = ''
+    if (stringItems[0].length <= MAX_LENGTH) {
+        result = stringItems[0]
+        for (let i = 1; i < stringItems.length; i++) {
+            if (result.length + stringItems[i].length > MAX_LENGTH) {
+                break
+            }
+            result += stringItems[i]
+        }
+        return result
+    }
+
     const ellipsisString = longString.slice(0, MAX_LENGTH)
     if (!/[。!]$/.test(ellipsisString)) {
         return ellipsisString + '...'
@@ -20,10 +33,10 @@ async function getWikiExtracts(titles) {
     try {
         const encodingTitles = encodeURIComponent(titles)
         const response = await axios.get(QUERY_API + encodingTitles)
-        const data = response.data.query.pages
+        const pages = response.data.query.pages
 
-        const wiki = Object.values(data)[0]
-        if (data['-1'] || !wiki) {
+        const wiki = Object.values(pages)[0]
+        if (pages['-1'] || !wiki) {
             return 'nano没有找到：' + titles
         }
 
