1c3a6feb4dbd5c8db811e77604ec42a095217935
cbdyzj <cbdyzj@jianzhao.org>
2020-02-13 18:57:59 +0800

    Amend

--- notes/javascript/code/rename.js
+++ notes/javascript/code/rename.js
@@ -1,9 +1,19 @@
 const fs = require('fs')
 
-const fileNames = fs.readdirSync(__dirname)
-
-for (const fileName of fileNames) {
-    if (fileName === 'a.txt') {
-        fs.renameSync(fileName, 'b.txt')
+function replaceRename(regex) {
+    if (!regex) {
+        return
     }
+    const fileNames = fs.readdirSync(__dirname)
+
+    for (const fileName of fileNames) {
+        if (regex.test(fileName)) {
+            const newFileName = fileName.replace(regex, '')
+            fs.renameSync(fileName, newFileName)
+        }
+    }
+}
+
+if (require.main === module) {
+    replaceRename(/target/gi)
 }
