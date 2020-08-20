45868ad34868b76ae563c2b2661be5956ccd47bf
cbdyzj <cbdyzj@jianzhao.org>
2020-08-20 19:25:48 +0800

    Amend

--- java-code/src/main/java/draft/FileCutter.java
+++ java-code/src/main/java/draft/FileCutter.java
@@ -28,24 +28,27 @@ public class FileCutter {
     @SneakyThrows
     public List<String> split(String pathname) {
         var is = new FileInputStream(pathname);
-
-        var pathnameList = new ArrayList<String>();
-        for (int i = 1; ; i++) {
-            var partFilename = pathname + PART_SUFFIX + i;
-            var os = new FileOutputStream(partFilename);
-            byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
-            long transferred = 0;
-            int read;
-            while ((read = is.read(buffer, 0, DEFAULT_BUFFER_SIZE)) >= 0) {
-                os.write(buffer, 0, read);
-                transferred += read;
-                if (transferred + DEFAULT_BUFFER_SIZE > this.unitSize) {
+        try (is) {
+            var pathnameList = new ArrayList<String>();
+            for (int i = 1; ; i++) {
+                var partFilename = pathname + PART_SUFFIX + i;
+                var os = new FileOutputStream(partFilename);
+                try (os) {
+                    byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+                    long transferred = 0;
+                    int read;
+                    while ((read = is.read(buffer, 0, DEFAULT_BUFFER_SIZE)) >= 0) {
+                        os.write(buffer, 0, read);
+                        transferred += read;
+                        if (transferred + DEFAULT_BUFFER_SIZE > this.unitSize) {
+                            break;
+                        }
+                    }
+                }
+                pathnameList.add(partFilename);
+                if (read <= 0) {
                     break;
                 }
-            }
-            pathnameList.add(partFilename);
-            if (read <= 0) {
-                break;
             }
         }
         return pathnameList;
@@ -54,11 +57,13 @@ public class FileCutter {
     @SneakyThrows
     public void merge(List<String> partPathnameList, String pathname) {
         var os = new FileOutputStream(pathname);
-
-        for (String partPathname : partPathnameList) {
-            var is = new FileInputStream(partPathname);
-            is.transferTo(os);
+        try (os) {
+            for (String partPathname : partPathnameList) {
+                var is = new FileInputStream(partPathname);
+                try (is) {
+                    is.transferTo(os);
+                }
+            }
         }
     }
-
 }
