c302bd348d6ace1ab87355ffa0bf7dadf6b1305f
cbdyzj <cbdyzj@jianzhao.org>
2020-08-27 19:16:07 +0800

    Amend

--- java-code/notes/groovy/serve.groovy
+++ java-code/notes/groovy/serve.groovy
@@ -1,12 +1,15 @@
 #!/usr/bin/env groovy
-@Grab('io.vertx:vertx-web:3.9.2')
+@Grab('io.vertx:vertx-web:3.8.3')
 import io.vertx.core.Vertx
 
 def port = 8000
-Vertx.vertx().createHttpServer().requestHandler({ req ->
+
+def serve = { req ->
     req.response()
             .putHeader("content-type", "text/plain")
             .end("hello world")
-}).listen(port)
+}
 
-print("Vert.x server running on $port")
+Vertx.vertx().createHttpServer().requestHandler(serve).listen(port)
+
+print("Vert.x server running on $port\n")
